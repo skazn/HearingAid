5d1fdf7ec71fef756df12efc1585723c3ed0500d
Rye Loken <rloken90@gmail.com>
2016-04-13 12:29:52 -0600

    Initial Project

---
+++ app/src/main/java/com/activities/rloken/hearingaid/Home.java
@@ -0,0 +1,15 @@
+package com.activities.rloken.hearingaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.activities.rloken.hearingaid.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
